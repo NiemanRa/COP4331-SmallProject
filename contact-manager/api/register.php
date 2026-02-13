b283b8e01cc4f59e5ccfd34140a4db3490141b45
ParkerM6767 <parker@lamay.systems>
2026-02-12 22:22:27 -0500

    Fixed syntax error with SQL query and return type for checkUsername()

--- contact-manager/api/register.php
+++ contact-manager/api/register.php
@@ -11,6 +11,7 @@
  */
 
 declare(strict_types=1);
+use function components\cookies\createCookie;
 
 ini_set('display_errors', '0');
 ini_set('log_errors', '1');
@@ -26,59 +27,65 @@ header('Content-Type: application/json');
 
 require './components/db.php';
 
-if (!isset($username, $pass)){
-    http_response_code(500);
-    echo json_encode(["error" => "Username and password are required."]);
-    exit;
+function checkUsername(string $username): bool
+{
+    if (strlen($username) < 3)
+        return false;
+
+    return (bool) preg_match('/^[a-zA-Z0-9]+$/', $username);
 }
 
-function checkUsername(string $username):bool {
-    if (strlen($username) < 3) return false;
-
-    return !preg_match('/^[a-zA-Z0-9]/', $username);
-}
-
-function checkPassword(string $pass):bool {
-    if (strlen($pass) < 3) return false;
+function checkPassword(string $pass): bool
+{
+    if (strlen($pass) < 3)
+        return false;
 
     // decide on what characters should be in passwords.
     return true;
 }
 
-try{
+try {
     $username = $input["username"];
     $email = $input["email"];
     $firstName = $input["firstName"];
     $lastName = $input["lastName"];
     $password = $input["password"];
 
+    // Requires username and password
+
+    if (!isset($username, $password)) {
+        http_response_code(400);
+        echo json_encode(["error" => "Username and password are required."]);
+        exit;
+    }
+
     // in case anyone sends a request themselves
-    if (!checkUsername($username)){
-        http_response_code(401);
+    if (!checkUsername($username)) {
+        http_response_code(400);
         echo json_encode(["error" => "Invalid username."]);
         exit;
     }
 
-    if (!checkPassword($password)){
-        http_response_code(401);
+    if (!checkPassword($password)) {
+        http_response_code(400);
         echo json_encode(["error" => "Invalid password."]);
         exit;
     }
     //
 
-    $hash = password_hash($pass, PASSWORD_DEFAULT);
+    $hash = password_hash($password, PASSWORD_DEFAULT);
 
     // register account in db
-    /** @var \PDO $pdo */
-    $message = $pdo->prepare("INSERT INTO users (first_name, last_name, email, username, password_hash) VALUES (?, ?, ?, ?, ?,)");
+
+    $message = $pdo->prepare("INSERT INTO users (first_name, last_name, email, username, password_hash) VALUES (?, ?, ?, ?, ?);");
     $message->execute([$firstName, $lastName, $email, $username, $hash]);
 
     $userId = $pdo->lastInsertId();
 
     require('./components/cookies.php');
-    components\cookies\createCookie($userId);
+    createCookie($pdo, $userId);
     exit;
-}catch (\PDOException $e){
+} catch (\PDOException $e) {
     $errorCode = $e->getCode();
     http_response_code(500);
     switch ($errorCode) {
