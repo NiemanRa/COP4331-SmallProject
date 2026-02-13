c0b082952f5c09834dc340ac754600618c78faba
ParkerM6767 <parker@lamay.systems>
2026-02-12 22:38:03 -0500

    login and registration API functionality

--- contact-manager/api/login.php
+++ contact-manager/api/login.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+use function components\cookies\createCookie;
 
 ini_set('display_errors', '0');
 ini_set('log_errors', '1');
@@ -12,3 +13,36 @@ $input = json_decode(file_get_contents('php://input'), true);
 
 require './components/db.php';
 
+try {
+    $username = $input["username"];
+    $password = $input["password"];
+
+    if (!isset($username, $password)) {
+        http_response_code(400);
+        echo json_encode(["error" => "Username and password are required."]);
+        exit;
+    }
+
+    $message = $pdo->prepare("SELECT id, username, password_hash FROM users WHERE username = ?;");
+    $message->execute([$username]);
+    $user = $message->fetch(PDO::FETCH_ASSOC);
+
+    if (!$user || !password_verify($password, $user["password_hash"])) {
+        http_response_code(401);
+        echo json_encode(["error" => "InvalidCredentials"]);
+        exit;
+    } else {
+        require('./components/cookies.php');
+        createCookie($pdo, $user["id"]);
+        http_response_code(200);
+
+        header("Location: ../contacts", true);
+        exit;
+    }
+    
+
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(["error" => "An error occurred while trying to sign in."]);
+    exit;
+}
--- contact-manager/js/login.js
+++ contact-manager/js/login.js
@@ -4,4 +4,38 @@ async function onRegister() {
     let email = document.getElementById("email").value;
     let username = document.getElementById("registerUsername").value;
     let password = document.getElementById("registerPassword").value;
+
+
+    let req = new XMLHttpRequest();
+    req.open("POST", "../api/register.php", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.send(JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        username: username,
+        password: password
+    }));
+}
+
+async function onLogin() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let req = new XMLHttpRequest();
+    req.open("POST", "../api/login.php", true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.send(JSON.stringify({
+        username: username,
+        password: password
+    }));
+    req.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+                //window.location.href = "../contacts";
+            } else {
+                // Do alert bootstrap class div to indicate invalid credentials
+            }
+        }
+    }
 }
