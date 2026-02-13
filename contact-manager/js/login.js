00b4066492fe55657699e9de2a70761b92faea41
ParkerM6767 <parker@lamay.systems>
2026-02-12 21:50:24 -0500

    login js initial

---
+++ contact-manager/js/login.js
@@ -0,0 +1,7 @@
+async function onRegister() {
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("registerUsername").value;
+    let password = document.getElementById("registerPassword").value;
+}
