9451f9daaf603e23a81e432777bf8c051d2adfa1
ParkerM6767 <parker@lamay.systems>
2026-02-17 23:32:25 -0500

    Fixed error with relative path for db.php module

--- contact-manager/api/components/db.php
+++ contact-manager/api/components/db.php
@@ -1,5 +1,5 @@
 <?php
-$env = parse_ini_file("../../../.env");
+$env = parse_ini_file( __DIR__ . "/../../../../.env");
 $charset = "utf8";
 
 $dsn = "mysql:host={$env['HOST']};port={$env['PORT']};dbname={$env['DB_NAME']};charset=$charset";
